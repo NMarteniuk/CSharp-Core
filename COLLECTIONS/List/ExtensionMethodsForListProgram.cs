7f0bdc2f03954b395933eb008b924af9c6928e39
Nazar Marteniuk <nazar.marteniuk@gmail.com>
2024-03-05 11:20:37 +0100

    add list with extension methods task

---
+++ COLLECTIONS/List/ExtensionMethodsForListProgram.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    static class ExtensionMethodsForListProgram
+    {
+        public static List<int> GetIndexes(this List<int> numbers, int number)
+        {
+            if (!numbers.Contains(number) || numbers.Count() == 0)
+            {
+                throw new Exception($"List doesn't contain element {number}.");
+            }
+            List<int> result = new List<int>();
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] == number)
+                {
+                    result.Add(i);
+                }
+            }
+            return result;
+        }
+        public static void PrintList(this List<int> numbers)
+        {
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ COLLECTIONS/List/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+namespace List
+{
+    class MyProgram
+    {
+        public static void Position(List<int> numbers)
+        {
+            numbers.GetIndexes(5).PrintList();
+        }
+        public static void Remove(List<int> numbers)
+        {
+            numbers.RemoveAll(x => x > 20);
+            numbers.PrintList();
+        }
+        public static void Insert(List<int> numbers)
+        {
+            if (8 > numbers.Count)
+            {
+                throw new ArgumentOutOfRangeException("Count of List is less than 8.");
+            }
+            numbers.Insert(2, -5);
+            numbers.Insert(5, -6);
+            numbers.Insert(7, -7);
+            numbers.PrintList();
+        }
+        public static void Sort(List<int> numbers)
+        {
+            numbers.Sort();
+            numbers.PrintList();
+        }
+    }
+}
